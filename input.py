39942745bb37be5120d84297fadedd02a5c250db
franromeroe <franromeroeze@gmail.com>
2023-03-22 18:15:00 -0300

    repaso

---
+++ input.py
@@ -0,0 +1,5 @@
+nombre = input(" Ingrese su nombre: ")
+print("hola " + nombre)
+
+edad = input("Ingrese su edad: ")
+print("Tu edad es " + edad)
--- variables.py
+++ variables.py
@@ -2,7 +2,24 @@ nombre = "Fran Romero"
 print(type(nombre)) # type str (osea es una cadena)
 
 numero = 5
-print(type((numero))) # type int (osea es un dato numerico)
+print(type((numero))) # type int (osea es un dato numerico Int viene de ser un numero entero)
 
 flotante = 3.14
 print(type(flotante)) # type float (es un valor flotante osea un numero decimal)
+
+# mas variables
+texto = "Repaso python"
+nombre = "Fran"
+altura = 1.75
+numero = 2
+print(texto + " soy " + nombre + " y tengo de altura " + altura)
+
+# parsear una varible string  a numero entero
+print(int(numero))
+
+# parsear una variable strin a numero decimales
+print(float(numero))
+
+# convertir a cadena
+print(str(altura))
+
