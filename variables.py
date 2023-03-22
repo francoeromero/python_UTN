7076636c227cc3938a26064e9cada540993e6eb9
franromeroe <franromeroeze@gmail.com>
2023-03-22 16:31:45 -0300

    repaso

---
+++ variables.py
@@ -0,0 +1,8 @@
+nombre = "Fran Romero"
+print(type(nombre)) # type str (osea es una cadena)
+
+numero = 5
+print(type((numero))) # type int (osea es un dato numerico)
+
+flotante = 3.14
+print(type(flotante)) # type float (es un valor flotante osea un numero decimal)
