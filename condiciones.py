27c92025be4b4a63b1b4e7b75b5eb2ad60ddd2ce
franromeroe <franromeroeze@gmail.com>
2023-03-22 21:11:54 -0300

    repaso

---
+++ condiciones.py
@@ -0,0 +1,8 @@
+# condiciones
+altura = input("Ingrese altura: ")
+altura = int(altura)
+
+if altura > 170:
+    print("Eres alto")
+else:
+    print("Eres bajito")
---
+++ funciones.py
@@ -0,0 +1,6 @@
+def mostrarAltura():
+    nombre = "fran"
+    if nombre == "fran":
+        print("hola fran")
+    else:
+        print("hola desconocido")
