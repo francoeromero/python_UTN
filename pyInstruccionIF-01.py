9149a3ac7ad7ae3066ec7b2851868022d614c7a9
franromeroe <franromeroeze@gmail.com>
2023-03-23 23:39:53 -0300

    repaso

---
+++ pyInstruccionIF-01.py
@@ -0,0 +1,8 @@
+def mostrar():
+    edad = input("Ingrese edad: ")
+    edad = int(edad)
+    if edad > 17:
+        print("Usted es mayor de edad")
+    else:
+        print("Usted es menor de edad")
+mostrar()
