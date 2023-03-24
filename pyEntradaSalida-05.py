0f2459031d9e1e4a284d10a024ae039858b35edb
franromeroe <franromeroeze@gmail.com>
2023-03-23 23:06:32 -0300

    repaso

--- pyEntradaSalida-05.py
+++ pyEntradaSalida-05.py
@@ -1,4 +1,5 @@
 def sumar():
+    print("Suma")
     # pido un numero y almaceno a una variable
     numeroUno = input("Ingrese un numero: ")
     # parseo a un numero
@@ -12,3 +13,50 @@ def sumar():
     # muestro
     print(resultado)
 sumar()
+
+def resta():
+    print("Resta")
+    # pido un numero y almaceno a una variable
+    numeroUno = input("Ingrese un numero: ")
+    # parseo a un numero
+    numeroUno = int(numeroUno)
+    # pido un numero y almaceno a una variable
+    numeroDos = input("Ingrese otro numero: ")
+    # parseo a un numero
+    numeroDos = int(numeroDos)
+    # calculo
+    resultado = numeroUno - numeroDos
+    print(resultado)
+resta()
+
+def multiplicar():
+    print("Multiplicar")
+    # pido un numero y almaceno a una variable
+    numeroUno = input("Ingrese un numero: ")
+    # parseo a un numero
+    numeroUno = int(numeroUno)
+    # pido un numero y almaceno a una variable
+    numeroDos = input("Ingrese otro numero: ")
+    # parseo a un numero
+    numeroDos = int(numeroDos)
+    # calculo
+    resultado = numeroUno * numeroDos
+    # muestro
+    print(resultado)
+multiplicar()
+
+def dividir():
+    print("Dividir")
+    # pido un numero y almaceno a una variable
+    numeroUno = input("Ingrese un numero: ")
+    # parseo a un numero
+    numeroUno = int(numeroUno)
+    # pido un numero y almaceno a una variable
+    numeroDos = input("Ingrese otro numero: ")
+    # parseo a un numero
+    numeroDos = int(numeroDos)
+    # calculo
+    resultado = numeroUno / numeroDos
+    # muestro
+    print(resultado)
+dividir()
