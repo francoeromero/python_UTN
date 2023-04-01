eef96886eaa6d27fdba8c3b2842a5b1231197a2a
franromeroe <franromeroeze@gmail.com>
2023-04-01 01:24:21 -0300

    ejercicios python

--- 03-for/02-for.py
+++ 03-for/02-for.py
@@ -1,1 +1,19 @@
 # Dada una lista de palabras, imprimir la palabra más larga de la lista.
+
+# nombres = ["franco", "fernando", "katherine", "jorge"]
+# print(len(nombres[0]))
+
+
+# nombres = ["franco", "fernando", "katherine", "jorge"]
+# nombre_mas_largo = len(nombres[0])
+# print(nombre_mas_largo) # 6
+
+nombres = ["franco", "fernando", "katherine", "jorge"]
+nombre_mas_largo = nombres[2]
+nombre_mas_corto = nombres[0]
+for i in nombres:
+    if(len(i) > len(nombre_mas_largo)):
+        nombre_mas_largo = i
+    if(len(i) < len(nombre_mas_corto)):
+        nombre_mas_corto = i
+print("El nombre mas largo es : {0} y el nombre mas corto es: {1} ".format(nombre_mas_largo, nombre_mas_corto))
---
+++ 03-for/03-for.py
@@ -0,0 +1,63 @@
+# Dado un número entero n, imprimir la secuencia de números pares menores o iguales a n.
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# n_ingresado = input("Ingrese un numero: ")
+# n = int(n_ingresado)
+# for i in numeros:
+#     if(i % 2 == 0 and i <= n):
+#         print(i)
+
+
+
+# # al reves con los numeros impares
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# n_ingresado = input("Ingrese su numero: ")
+# n = int(n_ingresado)
+# for i in numeros:
+#     if(not(i % 2 == 0) and i <= n):
+#         print(i)
+
+
+# numeros = list(range(0, 11))
+# print(numeros)
+
+
+# n_ingresado = input("Ingrese un numero: ")
+# n = int(n_ingresado)
+# numeros = list(range(0, 11)) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in numeros:
+#     if(i % 2 == 0 and i <= n):
+#         print(i)
+
+
+
+# n_ingresado = input("Ingrese un numero limite: ")
+# n = int(n_ingresado)
+# numeros = list(range(0, 11))
+# for num in numeros:
+#     if(num % 2 == 0 and num <= n):
+#         print(num)
+
+
+
+numero_ingresado = input("Ingrese un numero: ")
+numero = int(numero_ingresado)
+lista = list(range(0, 11))
+for a in lista: 
+    if(a % 2 == 0 and a <= numero):
+        print(a)
+
+
+
+
+
+
+
+
+
+# n = input("Ingrese un numero: ")
+# n = int(n)
+# for i in numeros:
+#     if(i % 2 == 0 and i <= n):
+#         print(i)
