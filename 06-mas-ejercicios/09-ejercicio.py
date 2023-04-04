8cd6f3d1f52420e886b1a04645689243e5f1d581
franromeroe <franromeroeze@gmail.com>
2023-04-04 16:04:21 -0300

    practica python

---
+++ 06-mas-ejercicios/09-ejercicio.py
@@ -0,0 +1,6 @@
+# Dadas las siguientes listas:
+# edades = [25,36,18,23,45]
+# nombre = ["Juan","Ana","Sol","Mario","Sonia"]
+# y considerando que la posición en la lista corresponde a la misma
+# persona, mostar el nombre de la persona más joven
+
