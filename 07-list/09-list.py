21ea96f1d2b041d6af8d117863a4e075a27bfc30
franromeroe <franromeroeze@gmail.com>
2023-04-07 12:18:52 -0300

    practica python

--- 07-list/09-list.py
+++ 07-list/09-list.py
@@ -3,10 +3,17 @@
 # numero = input("Ingrese un numero: ")
 # numero = int(numero)
 numeros = [1, 3, 2, 5, 8, 0, 2, 6]
-i = 0
-while(i < len(numeros)):
-    if(i == 3):
-        print("el numero es {} y su posicion es: {}".format(i, numeros.index(i)))
-        break
-    i += 1
-    
+# i = 0
+# while(i < len(numeros)):
+#     numero_ingresado = input("Ingrese un numero : ")
+#     numero = int(numero_ingresado)
+#     if(i == numero):
+#         print("el numero es {} y esta en la lista su posicion es: {}".format(i, numeros.index(i)))
+#         break
+#     i += 1
+
+for i in numeros:
+    numero_ingresado = input("Ingrese un numero : ")
+    numero = int(numero_ingresado)
+    if(i == numero):
+        print("El numero {} esta en la lista su posicion es: {}".format(i, numeros.index(i)))
