a56e3527e02160a37a3ccc73d2eaa0f2599d1fdb
franromeroe <franromeroeze@gmail.com>
2023-04-11 17:43:25 -0300

    python

--- 07-list/12-list.py
+++ 07-list/12-list.py
@@ -2,9 +2,23 @@
 
 peliculas = ["Rey leon", "Matrix", "300"]
 lista_orden_inverso = []
-i = 2
-while(i > -1):
+# con while
+# i = 2
+# while(i > -1):
+#     lista_orden_inverso.append(peliculas[i])
+#     i -= 1
+# print(lista_orden_inverso)
+# con for 
+for i in range(2, -1, -1):
     lista_orden_inverso.append(peliculas[i])
-    i -= 1
 print(lista_orden_inverso)
 
+
+#TEORIA
+#reversed hace que imprimas de 2 a 0 
+# for i in reversed(range(3)):
+#     print(i)
+
+# si no queres usar reversed, usas range(inicio, final, direccion)
+# for i in range(2, -1, -1):
+#     print(i)
