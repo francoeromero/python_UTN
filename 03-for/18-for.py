70eb3f6e56f5673f2ffa3ef292c430068a9cce5d
franromeroe <franromeroeze@gmail.com>
2023-04-12 00:29:42 -0300

    python

--- 03-for/18-for.py
+++ 03-for/18-for.py
@@ -1,1 +1,13 @@
 # Dada una lista de números, imprimir la suma de los números en la lista que son mayores que el promedio de la lista.
+
+numeros = [1, 2, 3, 4, 5]
+suma = 0
+suma_mayores_promedio = 0
+numeros_mayores_promedio = []
+promedio = sum(numeros) / len(numeros)
+
+for i in numeros:
+    if(i > promedio):
+        suma_mayores_promedio += i
+        numeros_mayores_promedio.append(i)
+print("Los numeros mayores que el promedio son: {} y la suma de todos es de: {}".format(numeros_mayores_promedio, suma_mayores_promedio))
--- 03-for/19-for.py
+++ 03-for/19-for.py
@@ -1,1 +1,20 @@
 # Dada una lista de palabras, imprimir las palabras que tienen una letra mayúscula.
+
+palabras = ["HOLA", "holas", "Hola", "HELLO"]
+# for i in palabras:
+#     if(i.isupper()):
+#         print(i)
+
+#creo una lista vacia
+letras_mayusculas = []
+#hago un for que pase por todas las palabras
+for palabra in palabras:
+    #luego hago que en cada palabra pase por cada letra
+    for letra in palabra:
+        # si la letra tiene mayuscula se imprime y se agrega ala lista
+        if(letra.isupper()):
+            print(letra)
+            letras_mayusculas.append(letra)
+# imprimo la lista de letras mayusculas
+print(letras_mayusculas)
+
