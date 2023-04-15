1f1d7c9c1f28c753b154a714f3a0742a5d11f641
franromeroe <franromeroeze@gmail.com>
2023-04-15 11:04:22 -0300

    python

---
+++ Py-Ingreso.py/pyBucle-10.py
@@ -0,0 +1,6 @@
+preguntar = 's'
+while preguntar=='s':
+    numIngresado = input("Ingrese un numero")
+    while numIngresado:
+        numIngresado = input("ERROR! ingrese un numero: ")
+    preguntar=input("Quiere seguir? : ")
